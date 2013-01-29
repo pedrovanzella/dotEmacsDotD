b6f19fe2fb10bb45d2b01e24e5775a98462ab9e3
Pedro Vanzella <pedro@pedrovanzella.com>
2013-01-29 18:28:00 -0200

    initial commit

---
+++ init.el
@@ -0,0 +1,16 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Package list
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings starter-kit-ruby starter-kit-eshell) "Package list to be autoinstalled.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
